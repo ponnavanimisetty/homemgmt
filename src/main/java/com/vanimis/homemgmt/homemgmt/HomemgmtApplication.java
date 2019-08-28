f8110f5eb2f69116f053bf0916b96b4a74777bb7
Ponnaga Vanimisetti <Ponnaga_Vanimisetti@epam.com>
2019-08-28 16:43:14 +0530

    GIT Initialization

---
+++ src/main/java/com/vanimis/homemgmt/homemgmt/HomemgmtApplication.java
@@ -0,0 +1,13 @@
+package com.vanimis.homemgmt.homemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomemgmtApplication.class, args);
+	}
+
+}
